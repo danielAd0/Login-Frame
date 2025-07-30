4ccc1ddef907cdf6217156c7d13b30a8003c8e48
Daniel <145106436+danielAd0@users.noreply.github.com>
2025-07-30 12:01:09 +0200

    Add files via upload
    In this project we create a frame that can be used to solicit the login details of a user

---
+++ LoginFrameApp.java
@@ -0,0 +1,17 @@
+
+package loginframeapp;
+
+import za.ac.tut.ui.LoginFrame;
+
+/**
+ *
+ * @author Daniel Adoration
+ */
+public class LoginFrameApp {
+
+    public static void main(String[] args) {
+        //instantiation
+        new LoginFrame();
+    }
+    
+}
